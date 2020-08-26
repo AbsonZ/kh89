312c43bee3661f71371a7d66e863c3019a46d14b
AbsonZ <1148037184@qq.com>
2020-08-26 22:18:34 +0800

    本地Hello

---
+++ hellogit/src/main/java/cn/kgc/HelloWorld.java
@@ -0,0 +1,11 @@
+package cn.kgc;
+
+/**
+ * @author shkstart
+ * @date 2020/8/26-21:54
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello~~~");
+    }
+}
